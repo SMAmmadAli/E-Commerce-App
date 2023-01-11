d167bf222aa944ad60d8c259afa75d5c26a1a280
S M Ammad Ali <65030609+SMAmmadAli@users.noreply.github.com>
2023-01-11 08:11:06 -0800

    Wroking on Home Page

--- lib/Pages/splash_page.dart
+++ lib/Pages/splash_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -10,18 +12,17 @@ class SplashScreen extends StatelessWidget {
         children: [
           Container(
             width: MediaQuery.of(context).size.width * 1,
-            height: MediaQuery.of(context).size.height * 0.7,
+            height: MediaQuery.of(context).size.height * 0.75,
             child: Stack(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(top: 40.0),
+                  padding: const EdgeInsets.only(top: 20.0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       Image.asset(
                         'asset/image/Oval 1.png',
-                        width: 100,
-                        alignment: Alignment.centerRight,
+                        width: 160,
                       ),
                     ],
                   ),
@@ -34,7 +35,7 @@ class SplashScreen extends StatelessWidget {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top: 70.0),
+                  padding: const EdgeInsets.only(top: 70.0, left: 30),
                   child: Image.asset(
                     'asset/image/bg image.png',
                     alignment: Alignment.center,
@@ -42,7 +43,77 @@ class SplashScreen extends StatelessWidget {
                 ),
               ],
             ),
-          )
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                'Welcome to STStore !',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 24,
+                ),
+              )
+            ],
+          ),
+          Expanded(
+            child: Center(
+              child: Column(
+                children: [
+                  Container(
+                    child: const Text(
+                      'With long experience in the audio industry,',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: const Text(
+                      'we create the best quality products',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: 20,
+            decoration: BoxDecoration(
+              color: Color(0xffC6AB59),
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(right: 100.0),
+                  child: Text(
+                    'Get Started',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_right,
+                  color: Colors.white,
+                  size: 22,
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 70,
+          ),
         ],
       ),
     );
