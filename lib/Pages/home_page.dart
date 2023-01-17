c7ac6c3c5d3732e205c7e8df76d482b8ad701af0
S M Ammad Ali <65030609+SMAmmadAli@users.noreply.github.com>
2023-01-17 08:43:40 -0800

    Home Page Completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:st_store/widgets/home_page_widget/bottom_navbar.dart';
 
 class HomePage extends StatelessWidget {
   List<Map> products = [
@@ -27,6 +28,7 @@ class HomePage extends StatelessWidget {
           color: Color(0xff111111),
         ),
       ),
+      bottomNavigationBar: const BottomNavBar(),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -46,68 +48,72 @@ class HomePage extends StatelessWidget {
             const SizedBox(
               height: 16,
             ),
-            Container(
-              height: 250,
-              width: 205,
-              child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      width: 205,
-                      height: 250,
-                      child: Stack(
-                        children: [
-                          Positioned(
-                            top: 70,
-                            child: Container(
-                              height: 150,
-                              width: 205,
-                              decoration: const BoxDecoration(
-                                  color: Color(0xffF3F6F8),
-                                  borderRadius: BorderRadius.horizontal(
-                                      right: Radius.elliptical(32, 80),
-                                      left: Radius.elliptical(32, 80))),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: const [
-                                  Text(
-                                    'Speakers',
-                                    style: TextStyle(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w700),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    '100+ Product',
-                                    style: TextStyle(
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.w400,
-                                        color: Color(0xff171717)),
-                                  ),
-                                  SizedBox(
-                                    height: 20,
-                                  ),
-                                ],
+            Padding(
+              padding: const EdgeInsets.only(left: 35.0),
+              child: Container(
+                height: 250,
+                width: 250,
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: 205,
+                        height: 250,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              top: 70,
+                              child: Container(
+                                height: 150,
+                                width: 205,
+                                decoration: const BoxDecoration(
+                                    color: Color(0xffF3F6F8),
+                                    borderRadius: BorderRadius.horizontal(
+                                        right: Radius.elliptical(32, 80),
+                                        left: Radius.elliptical(32, 80))),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: const [
+                                    Text(
+                                      'Speakers',
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      '100+ Product',
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400,
+                                          color: Color(0xff171717)),
+                                    ),
+                                    SizedBox(
+                                      height: 20,
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                          const Positioned(
-                            child:
-                                Image(image: AssetImage("asset/image/mic.png")),
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                  separatorBuilder: (context, index) => const SizedBox(
-                        width: 16,
-                      ),
-                  itemCount: 3),
+                            const Positioned(
+                              child: Image(
+                                  image: AssetImage("asset/image/mic.png")),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) => const SizedBox(
+                          width: 16,
+                        ),
+                    itemCount: 3),
+              ),
             ),
             const Divider(
               color: Color(0xffF3F6F8),
+              thickness: 5,
             ),
             const Padding(
                 padding: EdgeInsets.only(top: 24.0),
@@ -123,49 +129,52 @@ class HomePage extends StatelessWidget {
             const SizedBox(
               height: 24,
             ),
-            Container(
-              height: 220,
-              width: 145,
-              child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      height: 220,
-                      width: 145,
-                      decoration: BoxDecoration(
-                          color: Color(0xffF3F6F8),
-                          borderRadius: BorderRadius.circular(32)),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Image(
-                            image: AssetImage('asset/image/mic.png'),
-                          ),
-                          Text(
-                            'Speakers',
-                            style: TextStyle(
-                                fontSize: 14, fontWeight: FontWeight.w700),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            '100+ Product',
-                            style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w400,
-                                color: Color(0xff171717)),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                  separatorBuilder: (context, index) =>
-                      const SizedBox(width: 16),
-                  itemCount: 3),
+            Padding(
+              padding: const EdgeInsets.only(left: 35.0),
+              child: Container(
+                height: 220,
+                width: 250,
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        height: 220,
+                        width: 145,
+                        decoration: BoxDecoration(
+                            color: Color(0xffF3F6F8),
+                            borderRadius: BorderRadius.circular(32)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Image(
+                              image: AssetImage('asset/image/mic.png'),
+                            ),
+                            Text(
+                              'Speakers',
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.w700),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              '100+ Product',
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0xff171717)),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) =>
+                        const SizedBox(width: 16),
+                    itemCount: 3),
+              ),
             )
           ],
         ),
---
+++ lib/widgets/home_page_widget/bottom_navbar.dart
@@ -0,0 +1,71 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+  int _pageController = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        BottomNavyBar(
+          selectedIndex: _selectedIndex,
+          showElevation: true, // use this to remove appBar's elevation
+          onItemSelected: (index) => setState(() {
+            _selectedIndex = index;
+          }),
+          items: [
+            BottomNavyBarItem(
+              icon: const Icon(Icons.bungalow_outlined),
+              title: const Text('Home'),
+              activeColor: const Color(0xffC6AB59),
+            ),
+            BottomNavyBarItem(
+              icon: const Icon(Icons.search),
+              title: const Text('Search'),
+              activeColor: const Color(0xffC6AB59),
+            ),
+            BottomNavyBarItem(
+              icon: const Icon(Icons.shopping_bag_outlined),
+              title: const Text('Shapping'),
+              activeColor: const Color(0xffC6AB59),
+            ),
+            BottomNavyBarItem(
+              icon: const Icon(Icons.person),
+              title: const Text('Profile'),
+              activeColor: const Color(0xffC6AB59),
+            ),
+          ],
+        )
+        // BottomNavigationBar(
+        //     showSelectedLabels: true,
+        //     currentIndex: 0,
+        //     elevation: 0,
+        //     fixedColor: Colors.white,
+        //     unselectedItemColor: Colors.black,
+        //     items: const <BottomNavigationBarItem>[
+        //       BottomNavigationBarItem(
+        //         icon: Icon(Icons.home_filled, size: 18),
+        //         label: "Home",
+        //       ),
+        //       BottomNavigationBarItem(
+        //           icon: Icon(Icons.search, size: 18), label: "Search"),
+        //       BottomNavigationBarItem(
+        //           icon: Icon(Icons.shopping_bag_outlined, size: 18),
+        //           label: 'Shapping'),
+        //       BottomNavigationBarItem(
+        //         icon: Icon(Icons.person, size: 18),
+        //         label: "Profile",
+        //       ),
+        //     ])
+      ],
+    );
+  }
+}
