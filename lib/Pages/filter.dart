cd427efa7ff568ff312f7ab586f767e15b0a3e43
S M Ammad Ali <65030609+SMAmmadAli@users.noreply.github.com>
2023-02-01 07:38:02 -0800

    done

---
+++ lib/Pages/filter.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:st_store/utils/color.dart';
+
+class FilterPage extends StatelessWidget {
+  const FilterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
